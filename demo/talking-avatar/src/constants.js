9109c4bfbab53a0c28185b4f9f0ad4111d76c4d5
cperos-xr <cperos.xr@gmail.com>
2024-01-14 16:00:16 -1000

    add custom files

--- demo/talking-avatar/src/constants.js
+++ demo/talking-avatar/src/constants.js
@@ -1,4 +1,4 @@
 export const SETTINGS = {
-    "CONVAI-API-KEY": "",
-    "CHARACTER-ID": "37b0ede8-9ee0-11ed-97ae-42010a80000d"
+    "CONVAI-API-KEY": "450555ccb3773ca55cbcde2d2fa885b5",
+    "CHARACTER-ID": "3290113c-ae97-11ee-99d9-42010a40000f"
 };
